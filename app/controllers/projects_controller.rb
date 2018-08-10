f49be296dacf228cdf27211d321889a0e127092c
rob111 <andrewboston10@gmail.com>
2018-08-10 18:22:05 -0400

    add create new project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,30 @@ class ProjectsController < ApplicationController
   def show
 
   end
+
+  def new
+    if current_user
+      @client = Client.find(current_user.id)
+      @project = Project.new
+    end
+  end
+
+  def create
+    @statuses = ['Proposed', 'Active', 'Completed', 'Canceled']
+    @client = Client.find(current_user.id)
+    @project = Project.create(project_params)
+    @project.client = @client
+
+    if @project.save
+      flash[:notice] = "Project added successfully."
+      redirect_to @client
+    else
+      flash.now[:alert] = @project.errors.full_messages.join(', ')
+      render :new
+    end
+  end
+
+  def project_params
+    params.require(:project).permit(:description, :status, :deadline, :price)
+  end
 end
