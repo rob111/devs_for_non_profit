04849c9c3554b13e4ab7bc82a7b8b59a9122b5bd
rob111 <andrewboston10@gmail.com>
2018-08-20 22:35:19 -0400

    Add seed file for clients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+client1 = Client.create!(
+  email: 'cindy@sa.com',
+  username: 'CindyCrawford',
+  full_name: 'Cindy Crawford',
+  company: 'Salvation Army',
+  password: '000000',
+  isadmin: false,
+  profile_photo: 'cindy_cr_photo.jpg',
+  avatar_url: '',
+  uid: '',
+  provider: '',
+  oauth_token: '')
+
+client2 = Client.create!(
+  email: 'liz@puppy-connection.com',
+  username: 'LizKebab',
+  full_name: 'Liza Kebab',
+  company: 'Puppy Connection',
+  password: '000000',
+  isadmin: false,
+  profile_photo: 'liz_kedab.jpg',
+  avatar_url: '',
+  uid: '',
+  provider: '',
+  oauth_token: '')
+
+clientInfo1 = ClientInfo.create!(
+  client: client1,
+  company_size: '100+',
+  description: 'Helping people in needs to save money on second hand goods and others services.',
+  rep_position: 'Project Manager')
+
+clientInfo2 = ClientInfo.create!(
+  client: client2,
+  company_size: '15',
+  description: 'Helping abandoned animals to find new owners.', 
+  rep_position: 'Owner')
