6309b2093283c650cc2ea2d7ea780f0137089ceb
rob111 <andrewboston10@gmail.com>
2018-09-15 05:10:17 -0400

    Add test for adding a new project

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ApplicationRecord
   has_many :collaborations
   has_many :developers, through: :collaborations
   belongs_to :client
+
+  validates :description, presence: true
+  validates :price, presence: true
 end
