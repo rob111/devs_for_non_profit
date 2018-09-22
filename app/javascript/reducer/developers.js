c50a28ec339203682dfdcd1269e0c9cf0032365b
rob111 <andrewboston10@gmail.com>
2018-09-22 14:30:43 -0400

    Finalizes redux flow for the developer page

--- app/javascript/reducer/developers.js
+++ app/javascript/reducer/developers.js
@@ -1,16 +1,20 @@
 import actionTypes from '../constants/actionTypes';
 
 const initialState = {
-  developer: {}
+  developer: {},
+  projects: [],
+  clients:[],
+  info: {},
+  current_user: {}
 };
 
  const developersReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_DEVELOPER:
-      return Object.assign({}, state, {developer: action.payload})
+      return Object.assign({}, state, action.payload)
       break;
     default:
-
+      return state;
   }
 
 }
